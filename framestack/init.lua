4b114c8a282d8aae57410814fc6e668cea7be283
shagu <ericmauser@googlemail.com>
2021-10-03 19:29:55 +0200

    framestack: use queue to realize draw order

--- framestack/init.lua
+++ framestack/init.lua
@@ -1,66 +1,93 @@
 framestack = {}
-framestack.layers = {}
+framestack.frames = {}
+framestack.queue = {}
 
-local function draw(layers)
-  if not layers then return end
+-- get absolute frame geometry (x,y,width,height)
+framestack.geom = function(frame)
+  if not frame.parent then return 0, 0, 0, 0 end
+  local x, y = framestack.geom(frame.parent)
+  x, y = x or 0, y or 0
+  return frame.x + x, frame.y + y, frame.width, frame.height
+end
+
+-- get boolean if a frame is visible
+framestack.visible = function(frame)
+  if not frame or frame == framestack then return true end
+  if not frame.show then return nil end
+  return framestack.visible(frame.parent)
+end
+
+local function iterate(frames)
+  if not frames then return end
+
+  -- reset frame draw queue
+  framestack.queue = {}
 
   -- iterate over all frame layers
-  for layer, frames in pairs(layers) do
-    for id, frame in pairs(frames) do
-      if frame.show then
-        -- save and transform coordinates
-        love.graphics.push()
-        love.graphics.translate(frame.x, frame.y)
+  for id, frame in pairs(frames) do
+    -- update frame
+    if frame.update then
+      frame:update()
+    end
 
-        -- update frame
-        if frame.update then
-          frame:update()
-        end
+    -- queue frame draw
+    if framestack.visible(frame) then
 
-        -- draw frame
-        if frame.draw then
-          frame:draw()
-        end
-
-        -- process frame layers
-        draw(frame.layers)
-
-        -- reset coordinates
-        love.graphics.pop()
-      end
+      framestack.queue[frame.layer] = framestack.queue[frame.layer] or {}
+      table.insert(framestack.queue[frame.layer], frame)
     end
   end
 end
 
+framestack.update = function(self)
+  -- update frames and create draw queue
+  iterate(self.frames)
+end
+
 framestack.draw = function(self)
-  draw(self.layers)
+  -- draw framestack queue
+  for layer, frames in pairs(framestack.queue) do
+    for id, frame in pairs(frames) do
+      -- save and transform coordinates
+      local x, y = framestack.geom(frame)
+      love.graphics.push()
+      love.graphics.translate(x, y)
+
+      -- draw frame
+      if frame.draw then
+        frame:draw()
+      end
+
+      -- reset coordinates
+      love.graphics.pop()
+    end
+  end
 end
 
 framestack.new = function(parent, layer, name)
-  local parent = parent or framestack
-  local layer = layer or 1
+  local parent = parent or framestack and nil
+  local layer = layer or parent.layer or 1
   local name = name or "<unnamed>"
 
   -- build default frame
   local frame = {
-    name = name,
     parent = parent,
-    show = true,
+    layer = layer,
+    name = name,
 
     x = 0,
     y = 0,
     width = 0,
     height = 0,
+    show = true,
 
-    layers = {},
     new = framestack.new,
     update = nil,
     draw = nil,
   }
 
   -- add frame to parent layers
-  parent.layers[layer] = parent.layers[layer] or {}
-  table.insert(parent.layers[layer], frame)
+  table.insert(framestack.frames, frame)
 
   return frame
 end
