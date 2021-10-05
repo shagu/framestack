39a86ea6500c4a650926187aa48dd34c48f1f854
shagu <ericmauser@googlemail.com>
2021-10-05 21:48:27 +0200

    main: clean up dummy file

--- main.lua
+++ main.lua
@@ -1,50 +1,42 @@
-F = require "framestack"
+-- this is an example file with the purpose of
+-- describing how framestack can be used.
 
-local metrics = {
-  sum = 0, count = 0,
-  scan = function(self, max)
-    self.max = max
-    self.time = love.timer.getTime()
-  end,
+-- include core functions
+require "framestack"
 
-  resolve = function(self)
-    self.sum = self.sum + love.timer.getTime() - self.time
-    self.count = self.count + 1
-
-    if self.count >= self.max then
-      print(string.format("%d iterations took %.4f seconds", self.count, self.sum))
-      self.count, self.sum = 0, 0
-    end
-  end,
-}
-
+-- register framestack to love update
 function love.update()
-  F:update()
+  framestack:update()
 end
 
+-- register framestack to love draw
 function love.draw()
-  metrics:scan(100)
-  F:draw()
-  metrics:resolve()
+  framestack:draw()
 end
 
-local green = F:new(1, "green")
-green.x, green.y = 20, 20
-green.draw = function()
-  love.graphics.setColor( 0, 1, 0, 1 )
-  love.graphics.rectangle("fill", 0, 0, 500, 500)
-end
-
-local child = green:new(3, "green-child")
-child.x, child.y = 50, 50
-child.draw = function()
-  love.graphics.setColor( 1, 0, 1, 1 )
-  love.graphics.rectangle("fill", 0, 0, 100, 100)
-end
-
-local orange = F:new(2, "orange")
-orange.x, orange.y = 0, 0
-orange.draw = function()
+-- create frame on layer 2
+local orange = framestack:new(2, "orange")
+orange.x, orange.y = 100, 100
+orange.width, orange.height = 200,200
+orange.draw = function(self, x, y, width, height)
   love.graphics.setColor( 1, .5, 0, 1 )
-  love.graphics.rectangle("fill", 0, 0, 100, 100)
+  love.graphics.rectangle("fill", 0, 0, width, height)
+end
+
+-- create frame on layer 1
+local green = framestack:new(1, "green")
+green.x, green.y = 25, 25
+green.width, green.height = 500, 500
+green.draw = function(self, x, y, width, height)
+  love.graphics.setColor( 0, 1, 0, 1 )
+  love.graphics.rectangle("fill", 0, 0, width, height)
+end
+
+-- create child frame on layer 3
+local blue = green:new(3, "blue (child of green)")
+blue.x, blue.y = 50, 50
+blue.width, blue.height = 100, 100
+blue.draw = function(self, x, y, width, height)
+  love.graphics.setColor( 0, 0, 1, 1 )
+  love.graphics.rectangle("fill", 0, 0, width, height)
 end
