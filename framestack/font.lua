5b42dbe7f991e3129890eb66b800f195af479aec
shagu <ericmauser@googlemail.com>
2021-10-19 22:27:53 +0200

    font: add basic font object template

---
+++ framestack/font.lua
@@ -0,0 +1,75 @@
+-- This module adds a basic font template.
+--
+-- Variables:
+--  frame.text = "foo bar"
+--    The text that should be written.
+--
+--  frame.font = love.graphics.newFont()
+--    The font object that shall be used.
+--
+--  frame.color = { r, g, b, a }
+--    The color of the font.
+--
+--  frame.align = "LEFT"
+--    The horizontal text align inside the frame.
+--    Can be "LEFT", "CENTER" or "RIGHT".
+--
+--  frame.valign = "TOP"
+--    The vertical text align inside the frame.
+--    Can be "TOP", "MIDDLE" or "BOTTOM".
+--
+--  frame.shadow = { pos = "B", color = { 0, 0, 0, 0 } }
+--    The shadow mode of the font. "pos" an one or a combination of:
+--    T = top, B = bottom, L = left, R = right
+--
+
+framestack.templates["font"] = function(frame)
+  -- set core variables
+  frame.text = ""
+  frame.font = love.graphics.newFont()
+  frame.color = { 1, 1, 1, 1 }
+  frame.align = "CENTER"
+  frame.valign = "MIDDLE"
+  frame.shadow = {
+    pos = "TBLR",
+    color = { 0, 0, 0, .75 },
+    offset = 1,
+  }
+
+  -- register a font render to draw
+  frame.render["font"] = function(frame, x, y, width, height)
+    local width = frame.font:getWidth(frame.text)
+    local height = frame.font:getHeight(frame.text)
+    local offx, offy = 0, 0
+
+    if frame.align == "CENTER" then
+      offx = frame.width/2 - width/2
+    elseif frame.align == "RIGHT" then
+      offx = frame.width - width
+    end
+
+    if frame.valign == "MIDDLE" then
+      offy = frame.height/2 - height/2
+    elseif frame.valign == "BOTTOM" then
+      offy = frame.height - height
+    end
+
+    love.graphics.setFont(frame.font)
+    love.graphics.setColor(unpack(frame.shadow.color))
+
+    frame.shadow.pos:gsub(".", function(c)
+      if c == "T" then
+        love.graphics.print(frame.text, offx, offy - frame.shadow.offset)
+      elseif c == "B" then
+        love.graphics.print(frame.text, offx, offy + frame.shadow.offset)
+      elseif c == "L" then
+        love.graphics.print(frame.text, offx - frame.shadow.offset, offy)
+      elseif c == "R" then
+        love.graphics.print(frame.text, offx + frame.shadow.offset, offy)
+      end
+    end)
+
+    love.graphics.setColor(unpack(frame.color))
+    love.graphics.print(frame.text, offx, offy)
+  end
+end
