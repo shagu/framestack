b77473e4e92940898799f0526c4856e60ce34162
shagu <ericmauser@googlemail.com>
2021-10-20 19:02:07 +0200

    main: adapt example to use events for draw

--- main.lua
+++ main.lua
@@ -13,50 +13,50 @@ function love.load()
   framestack:init()
 end
 
--- create frame on layer 2
+-- create mouse-enabled font-frame on layer 2
 local orange = framestack:new(2, "orange", "font")
 orange.text = "Example Text"
 orange.x, orange.y = 100, 100
 orange.width, orange.height = 200,200
-orange.draw = function(self, x, y, width, height)
+orange.mouse = true
+
+orange:on("draw", function(self, event, x, y, width, height)
   love.graphics.setColor( 1, .5, 0, 1 )
   love.graphics.rectangle("line", 0, 0, width, height)
-end
+end)
+
+orange:on("click", function()
+  orange.text = "Click Orange"
+end)
 
 -- create frame on layer 1
 local green = framestack:new(1, "green")
 green.x, green.y = 25, 25
 green.width, green.height = 500, 500
-green.draw = function(self, x, y, width, height)
+green:on("draw", function(self, event, x, y, width, height)
   love.graphics.setColor( 0, 1, 0, 1 )
   love.graphics.rectangle("fill", 0, 0, width, height)
-end
+end)
 
--- create child frame on layer 3
+-- create mouse-enabled child frame on layer 3
 local blue = green:new(3, "blue (child of green)")
 blue.x, blue.y = 50, 50
 blue.width, blue.height = 100, 100
-blue.draw = function(self, x, y, width, height)
+blue.mouse = true
+
+blue:on("draw", function(self, event, x, y, width, height)
   love.graphics.setColor( 0, 0, 1, 1 )
   love.graphics.rectangle("fill", 0, 0, width, height)
-end
-
--- add click to orange and blue
-orange.mouse = true
-orange:on("click", function()
-  print("Orange")
 end)
 
-blue.mouse = true
 blue:on("click", function()
-  print("Blue")
+  orange.text = "Click Blue"
 end)
 
--- run something on mouse enter/leave
 blue:on("enter", function()
-  print("Enter Blue")
+  orange.text = "Enter Blue"
 end)
 
 blue:on("leave", function()
-  print("Leave Blue")
+  orange.text = "Leave Blue"
 end)
